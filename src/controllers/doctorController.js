5a3d9b456b5711ea94a66dc5f643a7eda59f4b90
hoa <lethikieuhoa103@gmail.com>
2023-08-02 18:27:36 +0700

    API get top doctor and foreign key gender and position

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,20 @@
+const doctorService = require('../services/doctorService');
+let getTopDoctorHome = async (req, res) => {
+    let limit = req.query.limit;
+    if (!limit) limit = 10;
+    try {
+        let doctors = await doctorService.getTopDoctorHome(+limit);
+        // console.log();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errorCode: -1,
+            message: ' Error from server...'
+        });
+    }
+
+}
+module.exports = {
+    getTopDoctorHome: getTopDoctorHome
+}
--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,7 @@
 //import express from "express";
 const homeController = require('../controllers/homeController');
 const userController = require('../controllers/userController');
+const doctorController = require('../controllers/doctorController');
 const express = require('express');
 let router = express.Router();
 let initWebRoutes = (app) => {
@@ -23,6 +24,7 @@ let initWebRoutes = (app) => {
     router.delete('/api/delete-user', userController.handleDeleteUser);
 
     router.get('/api/allcode', userController.getAllCode);
+    router.get('/api/top-doctor-home', doctorController.getTopDoctorHome)
     return app.use("/", router);
 
 
